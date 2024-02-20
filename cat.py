153c8e9d664684d80e12354327641b016e2aa835
InnateEvil <devil60000@rambler.ru>
2024-02-20 15:43:22 +0300

    Initial commit

---
+++ cat.py
@@ -0,0 +1,15 @@
+# cat.py
+class Cat:
+    def __init__(self, name, gender, age):
+        self.name = name
+        self.gender = gender
+        self.age = age
+
+    def getName(self):
+        return self.name
+
+    def getGender(self):
+        return self.gender
+
+    def getAge(self):
+        return self.age
